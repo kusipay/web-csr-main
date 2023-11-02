cd8fc6fa43bcc80bee56a1373c250713ea9d233c
Adhemir Sabino <adhemirsabino@gmail.com>
2023-11-02 18:20:10 -0500

    fix bugs

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 function Users() {
   const [vista, setVista] = useState(0);
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setVista(newValue);
   };
   return (
